fec2e7c3783aab8f06c1563278889f0889aa3e60
mattgeneux <72926012+mattgeneux@users.noreply.github.com>
2025-01-23 13:50:02 +0000

    css

--- src/app/components/TrackSummary.tsx
+++ src/app/components/TrackSummary.tsx
@@ -5,9 +5,9 @@ export const TrackSummary = (track: Track) => {
         <div className="me-5">
             <img src={track.album.images[0].url} height="75" width="75" />
         </div>
-        <div className="">
-            <h1>{track.name}</h1>
-            <h2>{track.artists.map(a => a.name).join(", ")}</h2>
+        <div className="flex flex-col">
+            <div>{track.name}</div>
+            <div>{track.artists.map(a => a.name).join(", ")}</div>
         </div>
 
     </div>)
