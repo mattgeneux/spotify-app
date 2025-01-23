196a906e3af31b5f577d95fbd55f31616e9b0f6b
mattgeneux <72926012+mattgeneux@users.noreply.github.com>
2025-01-23 13:48:08 +0000

    css is hard

--- src/app/components/TopItems.tsx
+++ src/app/components/TopItems.tsx
@@ -9,34 +9,40 @@ export const TopItems = ({ items, callback }: { items: { artists?: Artist[], tra
     return (
 
         <div className="flex flex-col">
-            <div className="flex-row mb-5">
+            <div className="flex-row mb-5 px-40">
                 <button className={`bg-transparent ${items?.range == FetchRange.FOUR_WEEKS ? "text-blue-400" : "text-white"} font-semibold hover:text-blue-500 py-2 px-4 border border-white rounded`} onClick={() => callback(FetchRange.FOUR_WEEKS)}>Last month</button>
                 <button className={`bg-transparent ${items?.range == FetchRange.SIX_MONTHS ? "text-blue-400" : "text-white"} font-semibold hover:text-blue-500 py-2 px-4 border border-white rounded ml-5`} onClick={() => callback(FetchRange.SIX_MONTHS)}>Last 6 months</button>
                 <button className={`bg-transparent ${items?.range == FetchRange.ONE_YEAR ? "text-blue-400" : "text-white"} font-semibold hover:text-blue-500 py-2 px-4 border border-white rounded ml-5`} onClick={() => callback(FetchRange.ONE_YEAR)}>Last year</button>
             </div>
-            <div>
-                <h1>Top Artists</h1>
-                <ul>
-                    {
-                        items?.artists?.slice(0, 5).map((a, i) =>
-                            <li key={i} className="mb-2">
-                                <ArtistSummary {...a}></ArtistSummary>
-                            </li>)
-                    }
-                </ul>
+            <div className="px-40">
+                <div className="flex flex-row justify-start">
+                    <div className="pr-20 flex-col">
+                        <div>Top Artists</div>
+                        <ul>
+                            {
+                                items?.artists?.slice(0, 5).map((a, i) =>
+                                    <li key={i} className="mb-2">
+                                        <ArtistSummary {...a}></ArtistSummary>
+                                    </li>)
+                            }
+                        </ul>
+                    </div>
+
+                    <div className="pl-20 flex-col">
+                        <div>Top Songs</div>
+                        <ul>
+                            {
+                                items?.tracks?.slice(0, 5).map((t, i) =>
+                                    <li key={i} className="mb-2">
+                                        <TrackSummary {...t}></TrackSummary>
+                                    </li>)
+                            }
+                        </ul>
+                    </div>
+                </div>
             </div>
 
-            <div>
-                <h1>Top Songs</h1>
-                <ul>
-                    {
-                        items?.tracks?.slice(0, 5).map((a, i) =>
-                            <li key={i} className="mb-2">
-                                <TrackSummary {...a}></TrackSummary>
-                            </li>)
-                    }
-                </ul>
-            </div>
+
 
         </div>
     )
